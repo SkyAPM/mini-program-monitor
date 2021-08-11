c8eea432b108383fc9ad6988d87b96bf53b54280
Juntao Zhang <jtarch@163.com>
2021-08-11 11:22:48 -0500

    feat: get current route (#5)

--- src/rewrite/rewritePage.ts
+++ src/rewrite/rewritePage.ts
@@ -1,3 +1,14 @@
+import { store } from '@/store';
+
+const methods = {
+  onLoad() {
+    store.setOptions({ pagePath: this.route });
+  },
+  onShow() {
+    store.setOptions({ pagePath: this.route });
+  },
+};
+
 function rewritePage(): void {
   const originPage = Page;
   Page = function (
@@ -5,9 +16,15 @@ function rewritePage(): void {
       | WechatMiniprogram.Page.Options<WechatMiniprogram.Page.DataOption, WechatMiniprogram.Page.CustomOption>
       | WechatMiniprogram.Component.MethodOption,
   ) {
-    //todo
-
+    for (const methodName in methods) {
+      const customMethod = pageOptions[methodName];
+      pageOptions[methodName] = function (data) {
+        methods[methodName].call(this, data);
+        return customMethod && customMethod.call(this, data);
+      };
+    }
     return originPage(pageOptions);
   } as WechatMiniprogram.Page.Constructor;
 }
+
 export { rewritePage };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './is';
+export * from './util';
 export * from './helpers';
